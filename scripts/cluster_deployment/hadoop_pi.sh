d986be3612be5832c3aeef1fd54d9307524841f6
yangzonglin <806765278@qq.com>
2019-06-26 04:18:57 +0800

    a small bug fixed

--- scripts/cluster_deployment/hadoop_pi.sh
+++ scripts/cluster_deployment/hadoop_pi.sh
@@ -3,5 +3,5 @@
 source head.sh
 
 yarn jar \
-    ${HADOOP_PATH}/${HADOOP_FOLDER}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar
+    ${HADOOP_PATH}/${HADOOP_FOLDER}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.0.jar \
     pi 10 10
