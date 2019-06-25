15c7b2f307f015f27be7b244000c5dbae1f7545a
yangzonglin <806765278@qq.com>
2019-06-26 02:55:21 +0800

    modify path and node variables

--- scripts/cluster_deployment/head.sh
+++ scripts/cluster_deployment/head.sh
@@ -2,19 +2,20 @@
 
 # 在这里修改用户
 USER=hadoop
+software_dir=${HOME}/software/
 
 # 相关软件的目录和文件夹名
-JDK_PATH=/usr/local/jdk
+JDK_PATH=${software_dir}/jdk
 JDK_FOLDER=jdk1.7.0_15
-HADOOP_PATH=/usr/local/hadoop
+HADOOP_PATH=${software_dir}/hadoop
 HADOOP_FOLDER=hadoop-2.6.0
-SCALA_PATH=/usr/local/scala
+SCALA_PATH=${software_dir}/scala
 SCALA_FOLDER=scala-2.11.8
-SPARK_PATH=/usr/local/spark
+SPARK_PATH=${software_dir}/spark
 SPARK_FOLDER=spark-2.1.0-bin-hadoop2.6
 
 HADOOP_TMP_DIR=/var/hadoop
 
 # 修改集群节点
-MASTER_NODE=ccit20
-SLAVE_NODE_LIST="ccit21 ccit22"
+MASTER_NODE=amaxsrv06
+SLAVE_NODE_LIST="amaxsrv03"
