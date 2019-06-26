9dbf4bd323983b609c633f2eae9b7cd09bea80c2
yangzonglin <806765278@qq.com>
2019-06-26 19:31:11 +0800

    disable event logging

--- scripts/cluster_deployment/spark_pi.sh
+++ scripts/cluster_deployment/spark_pi.sh
@@ -5,5 +5,6 @@ source head.sh
 spark-submit \
   --master spark://${MASTER_NODE}:8070 \
   --class org.apache.spark.examples.SparkPi \
+  --conf spark.eventLog.enabled=false \
   ${SPARK_PATH}/${SPARK_FOLDER}/examples/jars/spark-examples_2.11-2.1.0.jar \
   10
