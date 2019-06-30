db15e4928620b6fe1a41ef0918efadb4ec5043d8
yangzonglin <806765278@qq.com>
2019-07-01 01:34:25 +0800

    add a useful script used to update configuration file of project in cluster

---
+++ scripts/proofread_script/update_project_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ../cluster_deployment/head.sh
+
+# 这个脚本只能在主节点执行
+if [[ $HOSTNAME != ${MASTER_NODE} ]]; then
+	echo "当前主机不是主节点（$MASTER_NODE）"
+	echo "这个脚本必须在主节点（$MASTER_NODE）执行"
+	exit -1
+fi
+
+for slave_node in ${SLAVE_NODE_LIST}
+do
+    ssh ${USER}@${slave_node} "mkdir -p ~/proofread_swjtu/proofread_config"
+    scp ~/proofread_swjtu/proofread_config/* ${USER}@${slave_node}:~/proofread_swjtu/proofread_config/
+done
