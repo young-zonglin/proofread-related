dc141243a9fd8639616f39a3d2b721971f13eafd
yangzonglin <806765278@qq.com>
2019-07-01 02:10:24 +0800

    add a proofreading script used to start streaming proofread service

---
+++ scripts/proofread_script/streaming_proofread.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ../cluster_deployment/head.sh
+
+jar=${HOME}/ProofreadAPI.jar
+HADOOP_HOME=${HADOOP_PATH}/${HADOOP_FOLDER}
+SPARK_HOME=${SPARK_PATH}/${SPARK_FOLDER}
+monitor_dir="hdfs://${MASTER_NODE}:9000/user/${USER}/proofread_monitor"
+temp_dir="hdfs://${MASTER_NODE}:9000/user/${USER}/proofread_temp"
+batchDuration=10
+
+${HADOOP_HOME}/bin/hdfs dfs -mkdir -p ${monitor_dir}
+${HADOOP_HOME}/bin/hdfs dfs -mkdir -p ${temp_dir}
+
+nohup ${SPARK_HOME}/bin/spark-submit \
+    --master spark://${MASTER_NODE}:8070 \
+    --conf spark.memory.fraction=0.2 \
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+    --conf spark.eventLog.enabled=false \
+    --total-executor-cores 40 \
+    --executor-cores 20 \
+    --executor-memory 20g \
+    --driver-memory 2g \
+    --class com.swjtu.ccit.Service.parallel.StreamingProofread \
+    $jar 1>out.file 2>err.file &
+#$monitorDir $batchDuration \
+
+#--driver-java-options -XX:+UseConcMarkSweepGC \
+#--conf spark.executor.extraJavaOptions=-XX:+UseConcMarkSweepGC \
+
+#--conf spark.kryo.registrationRequired=true \
+#--conf spark.speculation=true \
