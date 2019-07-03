17a19569d6bf95d4260ee5ec57291d3e90f48c60
yangzonglin <806765278@qq.com>
2019-07-03 20:19:25 +0800

    disable spark locality and tune resources used by streaming proofreading service

--- scripts/proofread_script/streaming_proofread.sh
+++ scripts/proofread_script/streaming_proofread.sh
@@ -15,10 +15,11 @@ nohup ${SPARK_HOME}/bin/spark-submit \
     --conf spark.memory.fraction=0.2 \
     --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
     --conf spark.eventLog.enabled=false \
-    --total-executor-cores 40 \
-    --executor-cores 20 \
-    --executor-memory 20g \
-    --driver-memory 2g \
+    --conf spark.locality.wait=0 \
+    --driver-memory 4g \
+    --num-executors 6 \
+    --executor-cores 6 \
+    --executor-memory 8g \
     --class com.swjtu.ccit.Service.parallel.StreamingProofread \
     ${api_jar} ${monitor_dir} ${batch_duration} 1>out.file 2>err.file &
 
