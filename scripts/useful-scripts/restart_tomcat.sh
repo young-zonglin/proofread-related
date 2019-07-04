569d436497b5f7982d268f5abb642ead0b508e9e
yangzonglin <806765278@qq.com>
2019-07-04 22:50:34 +0800

    fix a bug

--- scripts/useful-scripts/restart_tomcat.sh
+++ scripts/useful-scripts/restart_tomcat.sh
@@ -6,10 +6,10 @@ source ../cluster_deployment/head.sh
 TOMCAT_HOME=${software_dir}/tomcat/tomcat-7.0.75/
 process_name=tomcat
 
-${TOMCAT_HOME}/bin/shutdown.sh
 get_process_num ${process_name}
 number=$?
 if [[ ${number} -ne 0 ]]; then
+    ${TOMCAT_HOME}/bin/shutdown.sh
 	echo `get_process_ID ${process_name}` | xargs kill -9
 fi
 
