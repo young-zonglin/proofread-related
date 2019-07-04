666f06d07e9df11d56d47230cf0f670c0f102486
yangzonglin <806765278@qq.com>
2019-07-04 19:52:37 +0800

    add a useful script used to restart streaming proofreading service

---
+++ scripts/useful-scripts/restart_streaming_proofreading.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source func.sh
+
+shell_scripts_dir=~/proofread_swjtu/proofread-related/scripts/proofread_script/
+process_name="StreamingProofread"
+
+get_process_num ${process_name}
+number=$?
+if [[ ${number} -ne 0 ]]; then
+	echo `get_process_ID ${process_name}` | xargs kill -9
+fi
+
+cd ${shell_scripts_dir}
+bash streaming_proofread.sh
+
