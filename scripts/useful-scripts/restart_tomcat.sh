dd49d912eb7650e1faad4528a5ac785fe2b40329
yangzonglin <806765278@qq.com>
2019-07-04 19:52:06 +0800

    add a useful script used to restart Tomcat Web server

---
+++ scripts/useful-scripts/restart_tomcat.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source func.sh
+source ../cluster_deployment/head.sh
+
+TOMCAT_HOME=${software_dir}/tomcat/tomcat-7.0.75/
+process_name=tomcat
+
+${TOMCAT_HOME}/bin/shutdown.sh
+get_process_num ${process_name}
+number=$?
+if [[ ${number} -ne 0 ]]; then
+	echo `get_process_ID ${process_name}` | xargs kill -9
+fi
+
+${TOMCAT_HOME}/bin/startup.sh
+
