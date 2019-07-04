3434925c45ac3c93c052153063ad52a83b841e6d
yangzonglin <806765278@qq.com>
2019-07-05 00:06:10 +0800

    also restart spark standalone cluster

--- scripts/useful-scripts/restart_streaming_proofreading.sh
+++ scripts/useful-scripts/restart_streaming_proofreading.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source func.sh
+source ../cluster_deployment/head.sh
 
 shell_scripts_dir=~/proofread_swjtu/proofread-related/scripts/proofread_script/
 process_name="StreamingProofread"
@@ -10,6 +11,9 @@ number=$?
 if [[ ${number} -ne 0 ]]; then
 	echo `get_process_ID ${process_name}` | xargs kill -9
 fi
+
+${SPARK_PATH}/${SPARK_FOLDER}/sbin/stop-all.sh
+${SPARK_PATH}/${SPARK_FOLDER}/sbin/start-all.sh
 
 cd ${shell_scripts_dir}
 bash streaming_proofread.sh
